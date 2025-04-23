710d4cf260e79a725bf940faa64ab98a872eaa19
Bibek Rai <168520229+raiTechie28@users.noreply.github.com>
2025-04-23 22:33:40 +0530

    Delete Day-9 directory

--- Day-9/client/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  extends: ["eslint:all", "plugin:react/all"],
-});
